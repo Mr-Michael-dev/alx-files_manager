a672cf55770965563ee513e99eee9e8edf985629
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-03 00:35:14 +0100

    (mod): properly handled the appController object to use a static object insead of creating a server

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,13 +1,19 @@
-import express from 'express';
 import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-app = express();
-
-app.get('/status', (req, res) => {
-  if (redisClient.isAlive()) {
-    res.status(200).json({ redis: true, db: true });
+// Used an object to create a static object for storing the route object
+// instead of using express
+// and then making an async call to the function to make it exportable
+class AppController {
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.status(200).json({ redis: true, db: true });
+    }
   }
-})
-app.get('/stats', (req, res) => {
-  res.status(200).json({"": 200})
-})
+
+  static async getStats(req, res) {
+    res.status(200).json({users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
+  }
+}
+
+export default AppController;
