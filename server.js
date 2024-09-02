c2a38b5ae51dbed61e7fcbc0f21290e311dc6d2e
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-03 00:37:23 +0100

    (mod): handled the port to accept both env port number or use a default of port 5000, added comments

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import router from './routes/index';
+import process from 'process'
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
-
+// Middleware to control all request sent into a JSON format and maps the route
+// blueprint to app
 app.use(express.json());
 app.use(router);
 
-
+// starts ports to run and listen on the specified port
 app.listen(port, () => {
   console.log("Server running on port 5000");
 });
