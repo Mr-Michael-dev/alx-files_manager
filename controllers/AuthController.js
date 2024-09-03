513c6c97ab0b47cf23df8e6bd03f82594f236b87
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-03 20:57:45 +0100

    (feat): added a disconnect method to the Auth controller but not fully implemented as there is still presence of bugs

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -14,11 +14,22 @@ class AuthController {
     const user = await dbClient.users.findOne({ email });
     if (!user) {
       res.status(401).json({ error: 'Unauthorized' });
-    } else if (user && sha1(user.password) === password) {
+    } else if (user && sha1(password) === user.password) {
       const userUUID = uuid4();
       const key = `auth_${userUUID}`;
       await redisClient.set(key, user, 24 * 60 * 60);
       res(200).json({ token: '155342df-2399-41da-9e8c-458b6ac52a0c' });
+    }
+  }
+
+  static async getDisconnect(req, res) {
+    // search for the x-token header to see if token is present & delete it
+    const token = req.headers['X-Token'];
+    if (token === undefined) {
+      res.status(401).json({ error: 'Unauthorized' });
+    } else {
+      await redisClient.del(token);
+      res.status(201);
     }
   }
 }
