ca351179c4c8e6e303707d0bd9118ace0fa73dd4
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-03 21:48:03 +0100

    (fix): modified eslint errors

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,29 +4,29 @@ import dbClient from '../utils/db';
 class UsersController {
   static async postNew(req, res) {
     try {
-    const { email, password } = req.body;
-    if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-    }
-    if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-    } 
-    
-    const existingUser = await dbClient.users.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ error: 'Already exists' });
-    }
-    // hash the password
-    const hashedPassword = sha1(password);
+      const { email, password } = req.body;
+      if (!email) {
+        res.status(400).json({ error: 'Missing email' });
+      }
+      if (!password) {
+        res.status(400).json({ error: 'Missing password' });
+      }
 
-    // insert new user
-    const result = await dbClient.users.insertOne({ email, password: hashedPassword });
-    res.status(201).json({ id: result.insertedId, email });
-  } catch(err) {
-    console.error('Error adding user:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
+      const existingUser = await dbClient.users.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: 'Already exists' });
+      }
+      // hash the password
+      const hashedPassword = sha1(password);
+
+      // insert new user
+      const result = await dbClient.users.insertOne({ email, password: hashedPassword });
+      res.status(201).json({ id: result.insertedId, email });
+    } catch (err) {
+      console.error('Error adding user:', err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
-}
 }
 
 export default UsersController;
