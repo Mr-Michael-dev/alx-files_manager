177ac91c77ef8bc7d2ec1889941b6788b4a1704b
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-09-04 13:45:37 +0100

    (fix): handled x-token instead of X-Token and check if the token found is null || undefined and removed the await

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,12 +1,13 @@
 import sha1 from 'sha1';
-import uuid4 from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AuthController {
   // AuthController.getConnect
   static async getConnect(req, res) {
-    const authHeader = req.headers.authorization;
+    // using the split method to parse a valid base64 string for atob
+    const authHeader = req.headers.authorization.split('Basic ')[1];
     // The atob is for splitting the base64 encoding and then converting it to a string
     // eslint-disable-next-line no-undef
     const [email, password] = atob(authHeader).split(':');
@@ -14,21 +15,26 @@ class AuthController {
     const user = await dbClient.users.findOne({ email });
     if (!user) {
       res.status(401).json({ error: 'Unauthorized' });
-    } else if (user && sha1(password) === user.password) {
-      const userUUID = uuid4();
+    }
+    if (user && sha1(password) === user.password) {
+      const userUUID = uuidv4();
       const key = `auth_${userUUID}`;
-      await redisClient.set(key, user, 24 * 60 * 60);
-      res(200).json({ token: userUUID.toString() });
+      await redisClient.set(key, JSON.stringify(user), 24 * 60 * 60);
+      res.status(200).json({ token: userUUID });
+    } else {
+      res.status(401).json({ error: 'Unauthorized' });
     }
   }
 
   static async getDisconnect(req, res) {
     // search for the x-token header to see if token is present & delete it
-    const token = req.headers['X-Token'];
-    if (token === undefined) {
+    const token = req.headers['x-token'];
+    const foundToken = await redisClient.get(`auth_${token}`);
+
+    if (!foundToken) {
       res.status(401).json({ error: 'Unauthorized' });
     } else {
-      await redisClient.del(`auth_${token}`);
+      redisClient.del(`auth_${token}`);
       res.status(201);
     }
   }
